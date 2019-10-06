4773c6bc6020fa459cfc1acadf148f03a4ca2bd8
ameerkabir <ameerkabir2@gmail.com>
2019-10-06 11:02:18 +0100

    Adding form custom hooks

---
+++ src/CustomeHooks.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+const useOpportunities = callback => {
+  const [inputs, setInputs] = useState({});
+  const handleSubmit = e => {
+    e.preventDefault();
+    callback();
+  };
+  const handleChange = e => {
+    e.persist();
+    setInputs(inputs => ({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  return {
+    handleSubmit,
+    handleChange,
+    inputs,
+  };
+};
+export default useOpportunities;
