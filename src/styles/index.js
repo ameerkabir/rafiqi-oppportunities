42e09528bcfd288d4fe0596a3f74b7c79b1b5d19
ameerkabir <ameerkabir2@gmail.com>
2019-10-13 19:35:01 +0100

    Moving shared styles to a style file.

---
+++ src/styles/index.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  width: 100%;
+  height: 40px;
+  margin-top: 10px;
+  border-radius: 5px;
+`;
+
+export const Select = styled.select`
+  width: 100%;
+  height: 40px;
+  margin-top: 10px;
+  border-radius: 5px;
+  background: white;
+`;
+
+export const Button = styled.button`
+  background: cyan;
+  font-size: 40px;
+  border-radius: 10px;
+`;
+export const Label = styled.label`
+  width: 100%;
+`;
+
+export const Form = styled.form`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+`;
