e7da465b998671b8525c855e077ca526d4c2725b
ameerkabir <ameerkabir2@gmail.com>
2020-01-18 17:38:11 +0000

    Adding prop types

--- src/components/DateInput.jsx
+++ src/components/DateInput.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import { Input } from '../styles';
 
 const DateInput = ({ onchange, value, name }) => {
   return <Input type="date" onChange={onchange} value={value} name={name} id={name} />;
+};
+
+DateInput.propTypes = {
+  onchange: propTypes.func.isRequired,
+  value: propTypes.string,
+  name: propTypes.string.isRequired,
 };
 export default DateInput;
